ca60426863b840686f6ef2b98b11c497582a16db
Dawid Wołosowicz <dawid.wolosowicz@polcode.net>
2015-03-24 11:47:41 +0100

    Prettyfing installation script.

--- install.sh
+++ install.sh
@@ -13,23 +13,22 @@ COLOR_NONE="\033[0m"
 ADMIN_USERNAME="admin"
 ADMIN_EMAIL="admin@example.com"
 
-echo -e $COLOR_NONE
-echo -e "$COLOR[#1] Creating Virtual Environment"
+echo -e "$COLOR[#1] Creating Virtual Environment$COLOR_NONE"
 virtualenv env
 
 source env/bin/activate
-echo -e $COLOR_NONE
-echo -e "$COLOR[#2] Virtual Environment created and activated"
+echo
+echo -e "$COLOR[#2] Virtual Environment created and activated$COLOR_NONE"
 
-echo -e $COLOR_NONE
-echo -e "$COLOR[#3] Installing dependencies..."
+echo
+echo -e "$COLOR[#3] Installing dependencies...$COLOR_NONE"
 pip install -r requirements.txt
 
-echo -e $COLOR_NONE
-echo -e "$COLOR[#4] Executing migrations, collecting static files"
+echo
+echo -e "$COLOR[#4] Executing migrations, collecting static files$COLOR_NONE"
 python project/manage.py migrate
 python project/manage.py collectstatic --noinput
 
-echo -e $COLOR_NONE
-echo -e "$COLOR[#5] Creating superuser with default username: $ADMIN_USERNAME and email: $ADMIN_EMAIL"
+echo
+echo -e "$COLOR[#5] Creating superuser with default username: $ADMIN_USERNAME and email: $ADMIN_EMAIL $COLOR_NONE"
 python project/manage.py createsuperuser --username=$ADMIN_USERNAME --email=$ADMIN_EMAIL
