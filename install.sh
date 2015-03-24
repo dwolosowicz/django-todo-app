bc1e5bd27b368edcd931af618c07b1ae78fcaa84
Dawid Wołosowicz <dawid.wolosowicz@polcode.net>
2015-03-24 11:03:48 +0100

    Deployment script.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+#
+# The installation script assumes a few things:
+# - the web server responsible for serving the app is properly configured for TODOApp
+# - the environment is python ready (means, python, pip and virtualenv are avaiable in PATH variable)
+# - the code repository has been successfully cloned into specific directory from which this script will run
+#
+
+virtualenv env
+source env/bin/activate
+python pip -r requirements.txt
+python project/manage.py migrate
