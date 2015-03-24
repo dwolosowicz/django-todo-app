3d2bc050e1d1c61844d97e77cb6addc2827dc760
Dawid Wołosowicz <dawid.wolosowicz@polcode.net>
2015-03-24 11:34:30 +0100

    Prettyfing installation script.

--- install.sh
+++ install.sh
@@ -7,21 +7,29 @@
 # - the code repository has been successfully cloned into specific directory from which this script will run
 #
 
+COLOR_LIGHTGREEN="\033[0;32m"
+COLOR_NONE="\033[0;0m"
+
 ADMIN_USERNAME="admin"
 ADMIN_EMAIL="admin@example.com"
 
-echo "[#1] Creating Virtual Environment"
+echo "$COLOR_LIGHTGREEN[#1] Creating Virtual Environment"
+echo $COLOR_NONE$
 virtualenv env
 
 source env/bin/activate
-echo "[#2] Virtual Environment created and activated"
+echo "$COLOR_LIGHTGREEN[#2] Virtual Environment created and activated"
+echo $COLOR_NONE$
 
-echo "[#3] Installing dependencies..."
+echo "$COLOR_LIGHTGREEN[#3] Installing dependencies..."
+echo $COLOR_NONE
 pip install -r requirements.txt
 
-echo "[#4] Executing migrations, collecting static files"
+echo "$COLOR_LIGHTGREEN[#4] Executing migrations, collecting static files"
+echo $COLOR_NONE
 python project/manage.py migrate
 python project/manage.py collectstatic --noinput
 
-echo "[#5] Creating superuser with default username: $ADMIN_USERNAME and email: $ADMIN_EMAIL"
+echo "$COLOR_LIGHGREEN[#5] Creating superuser with default username: $ADMIN_USERNAME and email: $ADMIN_EMAIL"
+echo $COLOR_NONE
 python project/manage.py createsuperuser --username=$ADMIN_USERNAME --email=$ADMIN_EMAIL
